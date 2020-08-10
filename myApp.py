1d683b3a03833ae91c0817575daff473f47da7ac
Daniel Berendeev <pipfstarrd@pipfstarrd.net>
2020-08-10 13:47:57 +0000

    Ooops, whitespaces

--- myApp.py
+++ myApp.py
@@ -18,44 +18,44 @@ help_message = "Required and integer argument to specify an action!\n\
 
 
 def connect_db(name):
-	return sqlite3.connect(name)
+    return sqlite3.connect(name)
 
 
 def get_cursor(connection):
-	return connection.cursor()
+    return connection.cursor()
 
 
 def close_db(connection):
-	connection.close()
+    connection.close()
 
 
 def show_help():
-	print(help_message)
-	exit(1)
+    print(help_message)
+    exit(1)
 
 
 def create_db(connection, cursor):
-	print("Creating table...")
-	cursor.execute("CREATE TABLE names\
+    print("Creating table...")
+    cursor.execute("CREATE TABLE names\
                       (name text, birthdate text, gender text)")
 
-	print("Done!")
+    print("Done!")
 
 
 def insert_records(connection, cursor, records):
-	# TODO: no input filtering, danger of SQL Injection
-	cursor.executemany("INSERT INTO names VALUES (?, ?, ?)", records)
-	connection.commit()
-	print("New record inserted.")
+    # TODO: no input filtering, danger of SQL Injection
+    cursor.executemany("INSERT INTO names VALUES (?, ?, ?)", records)
+    connection.commit()
+    print("New record inserted.")
 
 
 def show_unique(cursor):
-	for row in cursor.execute("SELECT * FROM names GROUP BY name, birthdate ORDER BY name"):
-		birthdate = datetime.strptime(row[1], '%d.%m.%Y')
-		current = date.today()
-		age = current.year - birthdate.year - ((current.month, current.day) <
-		                                       (birthdate.month, birthdate.day))
-		print(row[0] + "\t" + row[1] + "\t" + row[2] + "\t" + str(age))
+    for row in cursor.execute("SELECT * FROM names GROUP BY name, birthdate ORDER BY name"):
+        birthdate = datetime.strptime(row[1], '%d.%m.%Y')
+        current = date.today()
+        age = current.year - birthdate.year - ((current.month, current.day) <
+                                               (birthdate.month, birthdate.day))
+        print(row[0] + "\t" + row[1] + "\t" + row[2] + "\t" + str(age))
 
 
 def get_random_string(length):
@@ -64,77 +64,78 @@ def get_random_string(length):
 
 
 def generate_record():
-	name = (get_random_string(8).capitalize() + " " +
-	        get_random_string(6).capitalize() + " " +
-	        get_random_string(9).capitalize())
+    name = (get_random_string(8).capitalize() + " " +
+            get_random_string(6).capitalize() + " " +
+            get_random_string(9).capitalize())
 
-	birthdate = (str(random.randrange(1, 28)) + "." +
-	             str(random.randrange(1, 12)) + "." +
-	             str(random.randrange(1900, 2019)))
+    birthdate = (str(random.randrange(1, 28)) + "." +
+                 str(random.randrange(1, 12)) + "." +
+                 str(random.randrange(1900, 2019)))
 
-	gender = random.choice(('F', 'M'))
+    gender = random.choice(('F', 'M'))
 
-	return (name, birthdate, gender)
+    return (name, birthdate, gender)
 
 
 def autofill_dummy(connection, cursor):
-	records = []
-	for i in range (1000000):
-		records.append(generate_record())
+    records = []
+    for i in range (1000000):
+        records.append(generate_record())
 
-	insert_records(connection, cursor, records)
-	print("Done, 1,000,000 names inserted")
+    insert_records(connection, cursor, records)
+    print("Done, 1,000,000 names inserted")
 
 
 def show_males_with_f(cursor):
-	timestamp = time.time()
-	for row in cursor.execute("SELECT * FROM names WHERE gender='M' AND name LIKE 'f%'"):
-		print(row[0] + "\t" + row[1] + "\t" + row[2])
+    timestamp = time.time()
+    for row in cursor.execute("SELECT * FROM names WHERE gender='M' AND name LIKE 'f%'"):
+        print(row[0] + "\t" + row[1] + "\t" + row[2])
 
-	time_elapsed = time.time() - timestamp
-	print("Done in " + str(time_elapsed) + "s")
+    time_elapsed = time.time() - timestamp
+    print("Done in " + str(time_elapsed) + "s")
+
 
 def main():
-	# check if a mode to run is specified
-	try:
-		mode = int(sys.argv[1])
-	except (ValueError, IndexError):
-		show_help()
+    # check if a mode to run is specified
+    try:
+        mode = int(sys.argv[1])
+    except (ValueError, IndexError):
+        show_help()
 
-	if mode < 1 or mode > 5:
-		show_help()
+    if mode < 1 or mode > 5:
+        show_help()
 
-	# Open connection to the DB
-	connection = connect_db('names.db')
-	cursor = get_cursor(connection)
+    # Open connection to the DB
+    connection = connect_db('names.db')
+    cursor = get_cursor(connection)
 
 
-	# Do the specified action
-	if mode == 1:
-		create_db(connection, cursor) # Create DB
-	elif mode == 2:
-		try:
-			insert_records(connection, cursor, [(str(sys.argv[2]) + " " + str(sys.argv[3]) +
-			               " " + str(sys.argv[4]), str(sys.argv[5]),
-			               str(sys.argv[6]))]) # Insert record
-		except IndexError:
-			print("You need to specify the data in the following format:\n\
+    # Do the specified action
+    if mode == 1:
+        create_db(connection, cursor) # Create DB
+    elif mode == 2:
+        try:
+            insert_records(connection, cursor, [(str(sys.argv[2]) + " " + str(sys.argv[3]) +
+                           " " + str(sys.argv[4]), str(sys.argv[5]),
+                           str(sys.argv[6]))]) # Insert record
+        except IndexError:
+            print("You need to specify the data in the following format:\n\
 <SURNAME> <NAME> <SECOND NAME> <BIRTH DATE> <GENDER>\n\
 Birht date must be in format DD.MM.YYYY, e.g. 23.08.1986\n\
 Gender can be M or F.\n\
 Example input: Zhukov Oleg Petrovich 23.08.1986 M")
 
-	elif mode == 3:
-		show_unique(cursor) # Select unique
-	elif mode == 4:
-		autofill_dummy(connection, cursor) # Fill dummy data
-	elif mode == 5:
-		show_males_with_f(cursor) # Show males with names from F
+    elif mode == 3:
+        show_unique(cursor) # Select unique
+    elif mode == 4:
+        autofill_dummy(connection, cursor) # Fill dummy data
+    elif mode == 5:
+        show_males_with_f(cursor) # Show males with names from F
 
 
-	close_db(connection)
-	exit(0)
+    close_db(connection)
+    exit(0)
 
 
 if __name__ == '__main__':
-	main()
+    main()
